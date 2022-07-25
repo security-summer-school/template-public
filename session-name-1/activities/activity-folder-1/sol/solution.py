801f5fb75294bb7142132a4597959f26f8b948c7
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2022-07-26 01:59:23 +0300

    Update script to use double qoutes and adjust output
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- session-name-1/activities/activity-folder-1/sol/solution.py
+++ session-name-1/activities/activity-folder-1/sol/solution.py
@@ -4,14 +4,14 @@ import requests
 PORT = 8001
 
 if len(sys.argv) == 1:
-    print(f"Usage: {sys.argv[0]} {local,remote}\nor")
+    print(f"Usage: {sys.argv[0]} [local|remote]\nor")
     print(f"Usage: {sys.argv[0]} <ip> <port>")
     exit(1)
 
-if len(sys.argv) > 1 and sys.argv[1] == 'local':
-    HOST = '127.0.0.1'
-elif len(sys.argv) > 1 and sys.argv[1] == 'remote':
-    HOST = '141.85.224.103'
+if len(sys.argv) > 1 and sys.argv[1] == "local":
+    HOST = "127.0.0.1"
+elif len(sys.argv) > 1 and sys.argv[1] == "remote":
+    HOST = "141.85.224.103"
 else:
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
